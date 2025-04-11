73440de275ab08b348ffd2daaf9d980ae232e13f
Akshaya Shaji <akshayashaji66@gmail.com>
2025-04-11 15:48:37 +0530

    input box for task creation are created

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,29 @@
 import { createNavBar } from "../data/navbar.js"
 const dateEl = document.querySelector(".date")
 const navEl = document.querySelectorAll('.nav-tag')
-// const navBar = document.querySelector('.nav-container')
-
+const taskBtn = document.querySelector('.add-task-btn')
+const task=document.querySelector('.task-list-item')
+const taskContainer=document.querySelector('.main-container')
+getDate()
+createNavBar()
 function getDate() {
     const date = new Date()
     dateEl.innerHTML = date.toDateString()
 }
-getDate()
-createNavBar()
+taskBtn.addEventListener('click', () => {
+    createTask()
+})
+
+function createTask() {
+    task.innerHTML = `
+          <label for="name">Enter your task</label>
+          <input type="text" id="name" placeholder="Enter the task" class="name">
+          <label for="decription">write Description</label>
+          <input type="text" id="decription" placeholder="Description" class="description">
+          <label for="tag">Give tag</label>
+          <input type="text" id="tag" placeholder="tag" class="tag">
+          <button class="task-create-btn">Create</button>` 
+}
 
 
 
